a8d2c6efd7c0260a7982710785c33e5dc8661a06
ecobon-amjith <amjith4you@outlook.com>
2026-08-25 11:21:18 +0530

    Updates to include the code for getting the historical nav data

--- code/features/pofo_data.py
+++ code/features/pofo_data.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from features.pdf_process import MFtable
 import pdfplumber
-from features.amfi import  NAVFetcher
+from features.amfi_navhistory import  NAVFetcher
+from datetime import date, timedelta
 
 class MFdata:
 
@@ -14,37 +15,43 @@ class MFdata:
             password=self.password
         )
 
-    def mf_nav_fetch(self, nav_data):
-        
+    def mf_nav_fetch(self, target_date: date= date.today()-timedelta(days=1)):
+        nav_data = self.fetch.get_latest_nav(self.fetch._nearest_business_day(target_date))
         nav_df= self.fetch.get_processed_data(nav_data)
-        nav_df["ISIN"]=nav_df["ISINPrimary"] + nav_df["ISINReinvestment"]
+        nav_df["ISIN"]=nav_df["ISIN_RI"] + nav_df["ISIN_PO"]
 
         return nav_df
 
-    def get_pofo_value(self, lookup_columns:list = ["schemeName", "category", "ISIN", "date", "netAssetValue"]):
+    def get_pofo_value(self, 
+                    #    lookup_columns:list = ["schemeName", "category", "ISIN", "date", "netAssetValue"]
+                       lookup_columns:list = ["NAV_Name", "ISIN", "hNAV_Date", "hNAV_Amt"]
+                       ):
         pdf= MFtable(self.pdf_path, self.password)
-        df= pdf.mfc_table_df()
-        lookup_latest_nav_df = self.mf_nav_fetch(nav_data= self.fetch.get_latest_nav())
-        pf_data= df.merge(
+        nav_df= pdf.mfc_table_df()
+        lookup_latest_nav_df = self.mf_nav_fetch()
+        nav_pf_data= nav_df.merge(
             lookup_latest_nav_df[lookup_columns],
             on= "ISIN",
             how= "left"
         )
 
-        num_columns= ["netAssetValue"]
+        num_columns= ["hNAV_Amt"]
 
         # Strip out commas across all numeric columns
-        pf_data[num_columns] = pf_data[num_columns].replace("," , "",regex=True)
+        nav_pf_data[num_columns] = nav_pf_data[num_columns].replace("," , "",regex=True)
 
         # Convert all 3 columns to float
-        pf_data[num_columns] = pf_data[num_columns].apply(pd.to_numeric)
+        nav_pf_data[num_columns] = nav_pf_data[num_columns].apply(pd.to_numeric)
 
-        return pf_data
+        return nav_pf_data
 
     def get_processed_pofo_data(self,df_columns:list =["Scheme Name", "Cost Value", "Date", "NAV", "Market Value", "Gain", "Gain %", "All-Portfolio %"]):
         data= self.get_pofo_value()
-        data.rename(columns={"schemeId": "Scheme ID", "schemeName": "Scheme Name", "category": "Category", "date": "Date", "netAssetValue": "NAV"},inplace= True)
+        # data.rename(columns={"schemeId": "Scheme ID", "schemeName": "Scheme Name", "category": "Category", "date": "Date", "netAssetValue": "NAV"},inplace= True)
+        data.rename(columns={"schemeId": "Scheme ID", "NAV_Name": "Scheme Name", "category": "Category", "hNAV_Date": "Date", "hNAV_Amt": "NAV"},inplace= True)
 
+        data["Date"] = pd.to_datetime(data["Date"])
+        data["Date"]= data["Date"].dt.strftime('%Y-%m-%d')
         data["Market Value"]= data["Unit Balance"]*data["NAV"]
         data["Gain"]= data["Market Value"]-data["Cost Value"]
         data["Gain %"]= (data["Gain"]/data["Cost Value"])*100
--- code/features/streamlit_view.py
+++ code/features/streamlit_view.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from features.amfi import NAVFetcher
+from features.amfi_navhistory import NAVFetcher
 from features.pofo_data import MFdata
 
 st.title("Extract your portfolio from CAS")
@@ -9,14 +9,13 @@ pdf_pass = st.text_input(label= "Enter Password (If encrypted)", type= "password
 
 if st.button(label = "Extract"):
     if pdf_file is not None:
-        fetch= NAVFetcher(url="https://www.amfiindia.com/api/latest-nav?type=&mfid=all", cache_directory= r"data\daily_cache")
+        fetch= NAVFetcher(url="https://www.amfiindia.com/api/nav-history?query_type=all_for_date&from_date={date}", cache_directory= r"data\daily_cache")
         proc= MFdata(pdf_path= pdf_file, passw= pdf_pass, fetcher= fetch)
         df= proc.get_processed_pofo_data()
         st.write("### Your Mutual Fund Portfolio")
         st.metric(label= "Total Value", value= df["Market Value"].sum().astype(int), delta= df["Gain"].sum().astype(int))
         st.write(df.sort_values(by="Gain %", ascending= False).set_index("Scheme Name"))
         st.metric(label= "Cost Value", value= df["Cost Value"].sum().astype(int))
-        st.write(df)
 
     else:
         st.warning("Please attach a PDF file first before clicking the button.")
